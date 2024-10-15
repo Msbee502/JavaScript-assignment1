946d7ed0d6bfb2f8526c95e11d58622ca04c75ca
Msbee502 <b.hoogesteyn@gmail.com>
2024-10-15 17:05:14 +0200

    Adding Basic arithmetic operations

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let x = 'Beathrice';
-y = 44;
-z = 'rosa';
+console.log ( 'Hello world!' )
 
-console.log (`Hej jag heter ${x} och är ${y} år. Jag har många favoritfärger men en av dem är ${z}.`);
+let firstName ='Beathrice';
+year = 44;
+color = 'rosa';
+
+console.log (`Hej jag heter ${firstName} och är ${year} år. Jag har många favoritfärger men en av dem är ${color}.`);
+
+// let family = 4;
+// let family = family + 2;
+// let family = family - 1;
+// let family = family * 3;
+
+// family +=2;
+// family -=3;
+// family *=4;
+// family /=2;
+
+// family++;
+// family--;
+
+let family1 = 5
+let family2 = 10
+
+
+// console.log(family1 + family2);
+
+// console.log (family1-3);
+console.log (`I dessa två familjer finns det ${family1*family2/2} personer delat i två grupper`);
+
